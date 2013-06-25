98e11de011ed17061cebd099119c91a606b31de8
Rohit Rai <rohit@tuplejump.com>
2013-06-25 12:03:36 +0530

    minor fixes

---
+++ src/main/java/com/tuplejump/calliope/Cql3FileInputFormat.java
@@ -0,0 +1,11 @@
+package com.tuplejump.calliope;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rohit
+ * Date: 6/24/13
+ * Time: 3:32 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Cql3FileInputFormat extends InputFo{
+}
